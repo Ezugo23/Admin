db7d5e8159b89e22257f45c935d589df5bca70b0
Ezugo23 <134660097+Ezugo23@users.noreply.github.com>
2024-06-08 10:48:46 +0100

    new

--- src/Component/Profile/User.jsx
+++ src/Component/Profile/User.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import '../../style/user.css';
 import { IoMdCloseCircle } from "react-icons/io";
 
-export default function User() {
-  return (
-    <>
-  <div className="w-full max-w-4xl mx-auto">
-        <div className='border-none bg-white shadow-md mb-8 p-6' style={{marginTop:'30px'}}>
-        <p className='edit'>Add/Edit Personal Information</p>
-          <h5>Upload Image</h5>
-          <IoMdCloseCircle className='close-icon' />
-          <img src="https://bit.ly/dan-abramov" alt="" className='image2'/>
-          <button className='pro-btn'>Change Photo</button>
-        
-      </div>
-      </div>
-    </>
-  );
-}
+  export default function User() {
+    return (
+      <>
+        <div className="w-full max-w-4xl mx-auto">
+          <div className='border-none bg-white shadow-md mb-8 p-6' style={{ marginTop: '10px' }}>
+            <p className="font-roboto font-bold text-lg leading-6 text-black">Add/Edit Personal Information</p>
+            <hr className="mb-4 border-black" />
+            <h5>Upload Image</h5>
+            <div className='w-[343px] h-[364px] border-[0.1px] border-black relative flex flex-col items-center justify-center'>
+              <IoMdCloseCircle className='absolute top-1 right-2 cursor-pointer' />
+              <img src="https://bit.ly/dan-abramov" alt="" className='image2' />
+              <button className='pro-btn mt-2'>Change Photo</button>
+            </div>
+            
+          </div>
+        </div>
+      </>
+    );
+  }
