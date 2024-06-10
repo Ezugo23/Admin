d70c0fcf3efdadd236ab56dad5818a91d8108c44
Ezugo23 <134660097+Ezugo23@users.noreply.github.com>
2024-06-10 23:13:54 +0100

    new

--- src/pages/OrdersHistory/History.jsx
+++ src/pages/OrdersHistory/History.jsx
@@ -1,10 +1,14 @@
 import Order from '../../Component/History/Order';
 import { Routes, Route } from 'react-router-dom';
+import Reciept from '../../Component/History/Reciept'
 
 const History = () => {
   return(
   <>
-<Order />
+<Routes>
+<Route path='/' element={<Order/>}/>
+  <Route path='invoice' element={<Reciept/>}/>
+</Routes>
 </>
 )
 };
